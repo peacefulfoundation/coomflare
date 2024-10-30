4756abd86df09f89fa0de94c16a076937abc1399
Abhigyan Trips <contact@abhigyantrips.dev>
2024-10-30 07:46:58 +0530

    feat: init worker

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const url = new URL(request.url);
+		const key = url.pathname.slice(1);
+		const object = await env.bucket.get(key);
+
+		if (object === null) {
+			return new Response('Object Not Found', { status: 404 });
+		}
+
+		const headers = new Headers();
+		object.writeHttpMetadata(headers);
+		headers.set('etag', object.httpEtag);
+
+		return new Response(object.body, {
+			headers,
+		});
 	},
 } satisfies ExportedHandler<Env>;
